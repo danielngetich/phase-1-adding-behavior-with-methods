ee7ae550b0cb4a679a368371a653cb814688c6f8
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-08-29 09:44:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+class Cat{
+    constructor(name,sex){
+        this.name=name,
+        this.sex=sex,
+        this.speak =()=>{
+           return `${this.name} says meow!`
+        }
+    }
+}
+class Dog{
+    constructor(name,sex){
+        this.name=name,
+        this.sex=sex,
+        this.speak =()=>{
+           return `${this.name} says woof!`
+        }
+    }
+}
+class Bird{
+    constructor(name,sex){
+        this.name=name,
+        this.sex=sex,
+        this.speak =()=>{
+            if (this.sex==="male"){
+                return `It\'s me! ${this.name}, the parrot!`
+            } if (this.sex==="female"){
+                `${this.name} says squawk!`
+            }
+           return `${this.name} says squawk!`
+        }
+    }
+}
